c0acccfa7cf7659c23918e65d2a435959d10b3f9
Gabriel GonzaleZ <gaby_de_lomas7@hotmail.com>
2025-06-22 14:49:03 -0300

    Creo las carpetas y archivos para trabajarlos - modelo, servicio y componente 'ruta-alta'

---
+++ src/app/componentes/rutas-alta/rutas-alta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rutas-alta',
+  imports: [],
+  templateUrl: './rutas-alta.component.html',
+  styleUrl: './rutas-alta.component.css'
+})
+export class RutasAltaComponent {
+
+}
---
+++ src/app/modelos/ruta.ts
@@ -0,0 +1,8 @@
+export class Ruta {
+    id?: string;
+    nombre?: string;
+    origen?: string;
+    destino?: string;
+    distanciaKm?: number;
+
+}
